34136c55bd4d33ca9d888bed1c00819adeff7d71
Kary <starkary70@gmail.com>
2018-10-07 20:13:23 +0300

    Hello # Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	 const primeFactors = getPrimeFactors(base);
+  return Math.min(...primeFactors.map(getZerosCountForPrime.bind(null, number, base)));
+}
+ function getPrimeFactors(num) {
+  let primeFactors = [];
+   let divider = 2;
+  while (divider < num) {
+    if (num % divider === 0) {
+      num /= divider;
+      if (primeFactors.indexOf(divider) === -1) {
+        primeFactors.push(divider);
+      }
+    } else {
+      ++divider;
+    }
+  }
+  if (num !== 1) {
+    primeFactors.push(num);
+  }
+  return primeFactors;
+}
+ function getZerosCountForPrime(number, base, prime) {
+  let res = 0;
+   let maxFactorNumber = 0;
+  while (base % prime === 0) {
+    base /= prime;
+    ++maxFactorNumber;
+  }
+   while (number > 0) {
+    number = Math.floor(number / prime);
+    res += number;
+  }
+  
+  return Math.floor(res / maxFactorNumber);
+
 }
